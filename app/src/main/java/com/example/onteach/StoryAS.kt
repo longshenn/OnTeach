dde21a04cd45fa5f47f5b8f579770f451c936262
s1081702 <s1081702@gm.pu.edu.tw>
2021-06-25 00:43:02 +0800

    小純子弄南部

--- app/src/main/java/com/example/onteach/StoryAS.kt
+++ app/src/main/java/com/example/onteach/StoryAS.kt
@@ -1,11 +1,22 @@
 package com.example.onteach
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_story_as.*
+import kotlinx.android.synthetic.main.activity_storyac.*
 
 class StoryAS : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_story_as)
+
+        ress.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                intent = Intent(this@StoryAS , Start::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
